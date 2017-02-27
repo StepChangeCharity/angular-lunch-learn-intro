3ef4db7df5170c29fb3cceca820377a7249f6e18
ChrisMurphy <monkeeman69@googlemail.com>
2017-02-27 11:00:34 +0000

    feat(Store): Counter reducer & middleware

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
+
+import { INCREMENT, DECREMENT, RESET } from './counter';
+
+interface AppState {
+  counter: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  counter: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.counter = store.select('counter');
+  }
+
+  increment() {
+    this.store.dispatch({ type: INCREMENT });
+  }
+
+  decrement() {
+    this.store.dispatch({ type: DECREMENT });
+  }
+
+  reset() {
+    this.store.dispatch({ type: RESET });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { StoreModule, combineReducers } from '@ngrx/store';
+import { storeLogger } from 'ngrx-store-logger';
+import { compose } from "@ngrx/core/compose";
+
 import { AppComponent } from './app.component';
+import { counterReducer } from './counter';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore(
+      compose(storeLogger(), combineReducers)
+      ({ counter: counterReducer })
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
