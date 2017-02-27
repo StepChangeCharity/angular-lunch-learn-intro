ceba592fd10e1e658cb5d2607d2c8234f90d5fe5
ChrisMurphy <monkeeman69@googlemail.com>
2017-02-27 11:07:53 +0000

    feat(store): Redux dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { StoreModule, combineReducers } from '@ngrx/store';
 import { storeLogger } from 'ngrx-store-logger';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { compose } from "@ngrx/core/compose";
 
 import { AppComponent } from './app.component';
@@ -21,7 +22,11 @@ import { counterReducer } from './counter';
     StoreModule.provideStore(
       compose(storeLogger(), combineReducers)
       ({ counter: counterReducer })
-    )
+    ),
+    // Note that you must instrument after importing StoreModule
+    StoreDevtoolsModule.instrumentOnlyWithExtension({
+      maxAge: 5
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
